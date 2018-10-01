9a47855f8ff3eb65eb9865c3262e9db8372b7512
Wess58 <wesleymbate@gmail.com>
2018-10-01 10:19:22 +0300

    created tests

--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -1,5 +1,10 @@
 import org.junit.*;
-import static org.junit.*;
+import static org.junit.Assert.*;
 
 public class CeaserCipherTest {
+    @Test
+    public void str_isLowerCase(){
+        String str testStr = new str.toLowerCase ("BRO CODE ");
+        assertEquals("bro code", testStr instanceof str);
+    }
 }
